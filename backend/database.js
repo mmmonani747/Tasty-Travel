79b671f8e2f5921c9566d370aaee53cf7e3ecbd0
mmmonani747 <155767948+mmmonani747@users.noreply.github.com>
2024-10-01 20:01:57 +0530

    .

--- backend/database.js
+++ backend/database.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb+srv://user1:user1@cluster0.lymbh.mongodb.net/'
-const mongoDB =async() =>{
-await mongoose.connect(mongoURI, { useNewUrlParser: true }, async(err, result)=>{
-if(err) console.log("---", err)
-else{
-console.log("connected");
-const fetched_data = await mongoose.connection.db.collection("restaurants");
-fetched_data.find({}).toArray(function(err, data){
-if(err) console.log(err);
-else{
-console.log()
+const mongoDB = async () => {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+        if (err) console.log("---", err)
+        else {
+            console.log("connected");
+            const fetched_data = await mongoose.connection.db.collection("restaurants");
+            fetched_data.find({}).toArray(function (err, data) {
+                if (err) console.log(err);
+                else {
+                    console.log()
+                }
+            })
+        }
+    });
 }
-})
-}
-});}
