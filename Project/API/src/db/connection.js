8ec4db8fd927731399fb73c5c865c0841b39f3ca
DhyanMehta <152066218+DhyanMehta@users.noreply.github.com>
2024-11-15 18:56:00 +0530

    Delete Project directory

--- Project/API/src/db/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose= require('mongoose');
-mongoose.connect("mongodb://localhost:27017/Restaurant-Api")
-    .then(() => {
-        console.log("Connection successful");
-    })
-    .catch((e) => {
-        console.log("No connection", e);
-    });
--- Project/Backend/Routes/restaurantRoutes.js
+++
@@ -1,9 +0,0 @@
-// // Routes/restaurantRoutes.js
-// const express = require('express');
-// const { getAllRestaurants } = require('../controllers/restaurantController'); // Import the controller function
-// const router = express.Router();
-
-// // Define the route for getting all restaurants
-// router.route('/restaurants').get(getAllRestaurants);
-
-// module.exports = router;
